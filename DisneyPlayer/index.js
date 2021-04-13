00f6621bbddbed0af131e46031d6f96091f4a996
신유나 <yunadev01@gmail.com>
2021-04-13 17:10:09 +0900

    오디오 컨트롤

---
+++ DisneyPlayer/index.js
@@ -0,0 +1,36 @@
+import data from './play.js';
+
+const audio = document.querySelector(`audio[data-music='disney']`);
+const musicPlaying = (function () {
+  let isPlay = false;
+
+  return function () {
+    isPlay ? audio.pause() : audio.play();
+    isPlay = !isPlay;
+  };
+})();
+const styleChnage = (bg, id) => {
+  const keys = document.querySelectorAll('.key');
+  const background = document.querySelector('.bg');
+
+  keys.forEach(item => item.classList.remove('playing'));
+  background.style.backgroundImage = `url('${bg}')`;
+  keys[id].classList.add('playing');
+};
+
+window.addEventListener('keydown', e => {
+  if (e.keyCode === 32) {
+    musicPlaying();
+    return;
+  }
+
+  const key = document.querySelector(`div[data-key='${e.keyCode}']`);
+  if (!key) return;
+
+  const music = data.find(data => data.id == key.dataset.id);
+  const time = music.time.split(':').reduce((a, b) => Number(a) * 60 + Number(b));
+
+  audio.currentTime = time;
+  styleChnage(music.cover, music.id);
+  musicPlaying();
+});
